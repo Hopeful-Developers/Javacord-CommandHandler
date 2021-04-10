98a88702f18b42c919317d690e6781d7bce86366
Aurel <ballinaurel@gmail.com>
2021-04-10 23:30:41 +0200

    bump Javacord version to 3.2.0 added respond to CommandData

--- src/main/java/me/hopedev/commandhandler/CommandData.java
+++ src/main/java/me/hopedev/commandhandler/CommandData.java
@@ -2,7 +2,10 @@ package me.hopedev.commandhandler;
 
 import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class CommandData {
@@ -25,9 +28,13 @@ public class CommandData {
         return this.api;
     }
 
+    public final Message getMessage() { return this.getEvent().getMessage(); }
+
     public final MessageCreateEvent getEvent() {
         return this.messageCreateEvent;
     }
+
+    public final User getUser() { return this.getMessageAuthor().asUser().get(); }
 
     public final MessageAuthor getMessageAuthor() {
         return this.getEvent().getMessageAuthor();
@@ -44,6 +51,17 @@ public class CommandData {
     public final ServerTextChannel getChannel() {
         return this.getEvent().getServerTextChannel().get();
     }
+
+
+    public final void respond(EmbedBuilder eb) {
+        this.getMessage().reply(eb);
+    }
+
+    public final void respond(String message) {
+        this.getMessage().reply(message);
+    }
+
+
 
 
 }
